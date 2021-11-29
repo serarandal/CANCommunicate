faeaa2ab9981f465f4e15e09ab2d4550c7ee31b7
serarandal <serarandal@gmail.com>
2021-11-29 10:10:51 +0100

    Added sendMessage method

--- CanCommunicateWinVersion/Utility.py
+++ CanCommunicateWinVersion/Utility.py
@@ -1,4 +1,8 @@
 from ctypes import *
+from datetime import datetime
+
+class CANALMSG(Structure):
+    _fields_ = [("flags",c_ulong),("obid",c_ulong),("id",c_ulong),("count",c_char),("data",c_char_p*8),("timestamp",c_ulong)]
 #self.pushButton.clicked.connect(self.b1)
 #self.plainTextEdit.toPlainText()
 mydll = cdll.LoadLibrary("C:\\usb2can.dll")
@@ -8,3 +12,19 @@ def connectCan(serialNumber):
 
 def canGetStatus():
      return mydll.CanalGetStatus()
+
+def canSendMessage(id,count,data):
+    msg=CANALMSG()
+    msg.flags = '1000000000000000000000000000000'
+    msg.id = id
+    msg.count = count
+    for x in data:
+        msg.data[x] = data[x]
+    msg.timestamp = time()
+
+    return mydll.CanalSend(0,msg)
+
+def time():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    print(timestamp)
