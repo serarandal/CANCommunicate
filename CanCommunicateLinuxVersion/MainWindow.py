7eff82499650bc573fe902e31830a8a23ccc4e4d
serarandal <serarandal@gmail.com>
2021-11-30 10:51:29 +0100

    added new windows on the ui, TO DO implement the new features such as send messages through can

--- CanCommunicateLinuxVersion/MainWindow.py
+++ CanCommunicateLinuxVersion/MainWindow.py
@@ -12,6 +12,98 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import Utility
 
 
+class Ui_MainWindow3(object):
+    def setupUi(self, MainWindow3):
+        MainWindow3.setObjectName("MainWindow3")
+        MainWindow3.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow3)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.listView = QtWidgets.QListView(self.centralwidget)
+        self.listView.setObjectName("listView")
+        self.verticalLayout_3.addWidget(self.listView)
+        self.pushButton3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton3.setObjectName("pushButton3")
+        self.verticalLayout_3.addWidget(self.pushButton3)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(-1, 0, -1, -1)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton3_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton3_3.setObjectName("pushButton3_3")
+        self.gridLayout.addWidget(self.pushButton3_3, 0, 2, 1, 1)
+        self.pushButton3_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton3_2.setObjectName("pushButton3_2")
+        self.gridLayout.addWidget(self.pushButton3_2, 0, 0, 1, 1)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.gridLayout.addWidget(self.plainTextEdit, 0, 1, 1, 1)
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.gridLayout.addWidget(self.plainTextEdit_2, 0, 3, 1, 1)
+        self.verticalLayout_3.addLayout(self.gridLayout)
+        MainWindow3.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow3)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow3.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow3)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow3.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow3)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow3)
+
+    def retranslateUi(self, MainWindow3):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow3.setWindowTitle(_translate("MainWindow3", "MainWindow3"))
+        self.pushButton3.setText(_translate("MainWindow3", "PushButton"))
+        self.pushButton3_3.setText(_translate("MainWindow3", "PushButton"))
+        self.pushButton3_2.setText(_translate("MainWindow3", "PushButton"))
+
+
+
+
+
+class Ui_MainWindow2(object):
+    def setupUi(self, MainWindow2):
+        MainWindow2.setObjectName("MainWindow2")
+        MainWindow2.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow2)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.listView_2 = QtWidgets.QListView(self.centralwidget)
+        self.listView_2.setObjectName("listView_2")
+        self.gridLayout.addWidget(self.listView_2, 3, 0, 1, 1)
+        self.pushButton2_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton2_2.setObjectName("pushButton2_2")
+        self.gridLayout.addWidget(self.pushButton2_2, 4, 0, 1, 1)
+        self.pushButton2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton2.setObjectName("pushButton2")
+        self.gridLayout.addWidget(self.pushButton2, 1, 0, 1, 1)
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setObjectName("tableView")
+        self.gridLayout.addWidget(self.tableView, 0, 0, 1, 1)
+        MainWindow2.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow2)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow2.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow2)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow2.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow2)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow2)
+
+    def retranslateUi(self, MainWindow2):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow2.setWindowTitle(_translate("MainWindow2", "MainWindow2"))
+        self.pushButton2_2.setText(_translate("MainWindow2", "PushButton"))
+        self.pushButton2.setText(_translate("MainWindow2", "PushButton"))
+
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -26,20 +118,32 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName("ReadCan")
-        self.pushButton.clicked.connect(self.b1)
-        self.gridLayout.addWidget(self.pushButton, 2, 0, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.clicked.connect(self.b2)
-        self.gridLayout.addWidget(self.pushButton_2, 3, 0, 1, 1)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem, 1, 0, 1, 1)
+        self.gridLayout.addItem(spacerItem, 5, 0, 1, 1)
         self.listView = QtWidgets.QListView(self.centralwidget)
         self.listView.setObjectName("listView")
         self.listView.setModel(self.model)
         self.gridLayout.addWidget(self.listView, 0, 0, 1, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.clicked.connect(self.b3)
+        self.gridLayout.addWidget(self.pushButton_3, 6, 0, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem1, 1, 0, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.b2)
+        self.gridLayout.addWidget(self.pushButton_2, 3, 0, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.b1)
+        self.gridLayout.addWidget(self.pushButton, 2, 0, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.clicked.connect(self.b4)
+        self.gridLayout.addWidget(self.pushButton_4, 8, 0, 1, 1)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem2, 9, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
@@ -55,8 +159,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton_2.setText(_translate("MainWindow", "ReadCan"))
+        self.pushButton.setText(_translate("MainWindow", "ConnectCan"))
+        self.pushButton_3.setText(_translate("MainWindow", "SendPreMadeMessage"))
+        self.pushButton_4.setText(_translate("MainWindow", "SendManMessage"))
 
     def b1(self):
         print("Pulsado boton 1:")
@@ -64,14 +170,27 @@ class Ui_MainWindow(object):
         it = QtGui.QStandardItem(status)
         self.model.appendRow(it)
     def b2(self):
-        print("Pulsado boton 2")
+        print("Pulsado boton 2:")
         Utility.readCan()
+    def b3(self):
+        print("Pulsado boton 3:")
+        MainWindow2.show()
+    def b4(self):
+        print("Pulsado boton 4:")
+        MainWindow3.show()
+
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+    MainWindow2 = QtWidgets.QMainWindow()
+    MainWindow3 = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
+    ui2 = Ui_MainWindow2()
+    ui3 = Ui_MainWindow3()
     ui.setupUi(MainWindow)
+    ui2.setupUi(MainWindow2)
+    ui3.setupUi(MainWindow3)
     MainWindow.show()
     sys.exit(app.exec_())
